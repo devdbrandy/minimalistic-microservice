ca67afc82d82e9082fd828d36f5ee6b0799f6fd1
Harry Dbrandy <devdbrandy@gmail.com>
2020-04-09 04:16:38 +0100

    feat(auth): add authentication middleware 💚

---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,39 @@
+const createError = require('http-errors');
+const JWTService = require('../services/jwt-service');
+
+class AuthGuard {
+  /**
+   * A middleware to determine if the request is made by an authenticated user
+   *
+   * @param {Request} req - Express Request object
+   * @param {Response} res - Express Response object
+   * @param {Function} next - Calls the next middleware function in the stack
+   * @returns {object} HTTP response or moves to the next middleware
+   * @memberof AuthGuard
+   */
+  static async verifyToken(req, res, next) {
+    const authHeader = req.header('Authorization');
+
+    try {
+      if (!authHeader)
+        throw createError(403, 'Authentication token is required');
+
+      if (!authHeader.startsWith('Bearer')) {
+        throw createError(
+          401,
+          'Oops, authentication failed or token has expired.'
+        );
+      }
+
+      const token = authHeader.split(' ').pop();
+      const decoded = JWTService.verify(token);
+
+      req.user = decoded;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = AuthGuard;
--- src/modules/features/index.js
+++ src/modules/features/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 const controller = require('./features-controller');
+const { verifyToken } = require('../../middlewares/authenticate');
 
 /* POST generate thumbnail */
-router.post('/thumbnail', controller.createThumbnail);
+router.post('/thumbnail', verifyToken, controller.createThumbnail);
 
 /* POST generate jsonpatch */
-router.post('/jsonpatch', controller.jsonPatch);
+router.post('/jsonpatch', verifyToken, controller.jsonPatch);
 
 module.exports = router;
